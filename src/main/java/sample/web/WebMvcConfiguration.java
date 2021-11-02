6c3afd29285951a7f409525e0cff739301bcf1a9
tomasmoreira04 <tomasmoreira04@gmail.com>
2021-11-02 12:08:52 -0300

    aa

--- src/main/java/sample/web/WebMvcConfiguration.java
+++ src/main/java/sample/web/WebMvcConfiguration.java
@@ -1,35 +1,35 @@
 package sample.web;
 
-import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import sample.formatter.AmountFormatter;
 import sample.formatter.LocalDateTimeFormatter;
 
 @Configuration
-public class WebMvcConfiguration implements WebMvcConfigurer {
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
   @Override
   public void addFormatters(FormatterRegistry registry) {
+    super.addFormatters(registry);
     registry.addFormatter(new AmountFormatter());
     registry.addFormatter(new LocalDateTimeFormatter());
   }
 
   @Bean
-  public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>
-      webServerFactoryCustomizer() {
+  public EmbeddedServletContainerCustomizer containerCustomizer() {
     return new ServletContainerCustomizer();
   }
 
-  protected static class ServletContainerCustomizer
-      implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+  protected static class ServletContainerCustomizer implements EmbeddedServletContainerCustomizer {
 
-    public void customize(ConfigurableServletWebServerFactory factory) {
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer factory) {
       factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"));
     }
   }
